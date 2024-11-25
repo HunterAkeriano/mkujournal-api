a85d30c6b462e9b6edc2c688e107ed25fb822eed
VUE Дмитрий Гулак <stajectgamer@gmail.com>
2024-11-25 22:25:13 +0200

    master: add new endpoint

--- src/router/facultet.js
+++ src/router/facultet.js
@@ -1,32 +1,33 @@
 const {facultet, facultetItems, teachers} = require("../orm");
 const facultetRouter = require('express').Router();
 
+const Sequelize = require('sequelize');
 const Facultet = facultet
 const FacultetItem = facultetItems
 const Teachers = teachers
 
 
 facultetRouter.get('/select', async (req, res) => {
-   try {
-       const { search } = req.query;
+    try {
+        const { search } = req.query;
 
-       const result = await  Facultet.findAll()
+        const result = await  Facultet.findAll()
 
-       if(!search){
-           return res.json({
-               items: result,
-           })
-       }
+        if(!search){
+            return res.json({
+                items: result,
+            })
+        }
 
-       if(search){
-           const filteredResult = result.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
-           return res.json({
-               items: filteredResult,
-           })
-       }
-   }catch(err) {
-       res.status(400).send({error: err.message});
-   }
+        if(search){
+            const filteredResult = result.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+            return res.json({
+                items: filteredResult,
+            })
+        }
+    }catch(err) {
+        res.status(400).send({error: err.message});
+    }
 
 })
 
@@ -54,5 +55,41 @@ facultetRouter.get('/main-block', async (req, res) => {
         res.status(400).send({ error: err.message });
     }
 });
+
+facultetRouter.post('/update-survey', async (req, res) => {
+    try {
+        const { numberId } = req.body;
+
+        if (!numberId || !Array.isArray(numberId) || !numberId.length) {
+            return res.status(400).send({ message: 'Ви не відправили елементи' });
+        }
+
+
+        const updatedTeachers = await Teachers.update(
+            { voice: Sequelize.literal('voice + 1') },
+            {
+                where: {
+                    id: numberId
+                },
+                returning: true,
+                plain: false,
+            }
+        );
+
+
+        if (updatedTeachers[0] === 0) {
+            return res.status(404).send({ message: 'Жодного викладача не знайдено для оновлення' });
+        }
+
+        res.send({
+            message: 'Голоси викладачів успішно оновлені',
+            updatedTeachers: updatedTeachers[1] // The updated teachers
+        });
+
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+});
+
 
 module.exports = {facultetRouter};
